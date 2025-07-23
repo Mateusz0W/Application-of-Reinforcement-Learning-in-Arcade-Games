b25f566171436b987472d30e9a230bc1466d20ae
Mateusz0W <153832537+Mateusz0W@users.noreply.github.com>
2025-07-23 21:12:40 +0200

    add climbing on ladders

--- include/Obstacle.hpp
+++ include/Obstacle.hpp
@@ -2,9 +2,12 @@
 #include "Entity.hpp"
 
 class Obstacle: public Entity{
+    private:
+    std::string _type;
     public:
-        Obstacle(float x, float y, float width, float height): Entity(x,y,width,height){}
+        Obstacle(float x, float y, float width, float height,std::string type): Entity(x,y,width,height),_type(type){}
         void draw(sf::RenderWindow& window);
+        std::string getType();
 
 
 };
--- src/Obstacle.cpp
+++ src/Obstacle.cpp
@@ -1,8 +1,16 @@
 #include "Obstacle.hpp"
 
+using namespace std;
+
 void Obstacle::draw(sf::RenderWindow& window){
     sf::RectangleShape rectangle(sf::Vector2f(this->_width,this->_height));
-    rectangle.setFillColor(sf::Color::White);
+    if (this->_type == "platform")
+        rectangle.setFillColor(sf::Color::White);
+    else 
+        rectangle.setFillColor(sf::Color::Blue);
     rectangle.setPosition(sf::Vector2f(this->_dx,this->_dy));
     window.draw(rectangle);
+}
+string Obstacle::getType(){
+    return this->_type;
 }
