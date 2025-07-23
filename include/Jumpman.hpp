760becb47e8d5646855787cb20b7cffec463939b
Mateusz0W <153832537+Mateusz0W@users.noreply.github.com>
2025-07-23 21:10:43 +0200

    add jumping

--- include/Jumpman.hpp
+++ include/Jumpman.hpp
@@ -7,10 +7,12 @@ class Jumpman: public Entity{
         bool _keyboard;
         bool _rendering;
     public:
-        Jumpman():Entity(0,0,100,100),_keyboard(false),_rendering(false){}        
+        bool jumping;
+        Jumpman():Entity(0,0,100,100),_keyboard(false),_rendering(false),jumping(false){}        
         void move(std::string direction) override;
         void moveOnStairs(bool stairsContact);
         void draw(sf::RenderWindow& window);
+        void jump();
         CollisionBox getCollisionBox(std::string box);
 
 };
