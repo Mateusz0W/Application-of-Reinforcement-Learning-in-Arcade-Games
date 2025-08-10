93867cab51983933702293094531357c5275432c
Mateusz0W <153832537+Mateusz0W@users.noreply.github.com>
2025-08-10 18:21:55 +0200

    added window size to simulation constructor

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main(void){
     vector<unique_ptr<Entity>> entities;
     entities.push_back(make_unique<Jumpman>());
-    Simulation simulation(std::move(entities));
+    Simulation simulation(std::move(entities),1500,1700);
     simulation.loadMapFromJson("../map.json");
     Renderer renderer(simulation,1500,1700);
 
