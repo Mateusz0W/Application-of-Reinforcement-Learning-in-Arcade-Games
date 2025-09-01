34743aed3db3a9b9d29ddf9b62b83faa44a4ce98
Mateusz0W <153832537+Mateusz0W@users.noreply.github.com>
2025-09-01 19:19:32 +0200

    changed window size

--- DonkeyKong/Game/main.cpp
+++ DonkeyKong/Game/main.cpp
@@ -4,9 +4,9 @@ using namespace std;
 
 int main(void){
     Jumpman jumpman;
-    Simulation simulation(&jumpman,1500,1700);
+    Simulation simulation(&jumpman,1700,1500);
     simulation.loadMapFromJson(string(PROJECTPATH)+"/map.json");
-    Renderer renderer(simulation,1500,1700);
+    Renderer renderer(simulation,1700,1500);
 
     //simulation.run();
     renderer.run();
--- DonkeyKong/Game/main.py
+++ DonkeyKong/Game/main.py
@@ -1,6 +1,6 @@
 import build.Debug.DonkeyKongPy as DK
 jm = DK.Jumpman() 
-sim = DK.Simulation(jm,1500,1700)
+sim = DK.Simulation(jm,1700,1500)
 sim.loadMapFromJson("map.json")
-renderer = DK.Renderer(sim,1500,1700)
+renderer = DK.Renderer(sim,1700,1500)
 renderer.run()
