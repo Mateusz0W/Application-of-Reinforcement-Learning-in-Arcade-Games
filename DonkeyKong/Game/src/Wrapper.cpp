c376face13a4b490bd2a5546443fc9810d07e23c
Mateusz0W <153832537+Mateusz0W@users.noreply.github.com>
2025-09-01 19:26:53 +0200

    added more bindings for Simulation

--- DonkeyKong/Game/src/Wrapper.cpp
+++ DonkeyKong/Game/src/Wrapper.cpp
@@ -13,7 +13,11 @@ PYBIND11_MODULE(DonkeyKongPy, m){
     
     py::class_<Simulation>(m, "Simulation")
         .def(py::init<Jumpman*, unsigned int, unsigned int>())
-        .def("loadMapFromJson", &Simulation::loadMapFromJson);
+        .def("loadMapFromJson", &Simulation::loadMapFromJson)
+        .def("getBarrelsPositions", &Simulation::getBarrelsPositions)
+        .def("getJumpmanPosition", &Simulation::getJumpmanPosition)
+        .def("getReset", &Simulation::getReset)
+        .def("getWin", &Simulation::getWin);
 
     py::class_<Jumpman>(m, "Jumpman")
         .def(py::init<>());
