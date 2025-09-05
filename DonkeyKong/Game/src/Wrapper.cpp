939eda1fab090fabbec484cb6a9f21d5b7436b2b
Mateusz0W <153832537+Mateusz0W@users.noreply.github.com>
2025-09-05 19:18:22 +0200

    added stl.h + binded more simulation functions

--- DonkeyKong/Game/src/Wrapper.cpp
+++ DonkeyKong/Game/src/Wrapper.cpp
@@ -1,4 +1,5 @@
 #include<pybind11/pybind11.h>
+#include <pybind11/stl.h>
 #include "Renderer.hpp"
 #include "Simulation.hpp"
 #include "Entity.hpp"
@@ -13,13 +14,15 @@ PYBIND11_MODULE(DonkeyKongPy, m){
         .def("isOpen", &Renderer::isOpen);
     
     py::class_<Simulation>(m, "Simulation")
-        .def(py::init<Jumpman*, unsigned int, unsigned int>())
+        .def(py::init<Jumpman*, unsigned int, unsigned int, bool>())
         .def("loadMapFromJson", &Simulation::loadMapFromJson)
         .def("getBarrelsPositions", &Simulation::getBarrelsPositions)
         .def("getJumpmanPosition", &Simulation::getJumpmanPosition)
         .def("getReset", &Simulation::getReset)
         .def("getWin", &Simulation::getWin)
-        .def("run", &Simulation::run);
+        .def("run", &Simulation::run)
+        .def("restart", &Simulation::restart)
+        .def_readwrite("action", &Simulation::action);
 
     py::class_<Jumpman>(m, "Jumpman")
         .def(py::init<>());
