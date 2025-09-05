106c172fb8f2005ecf563ea45ef65d0eb00dba68
Mateusz0W <153832537+Mateusz0W@users.noreply.github.com>
2025-09-05 19:17:27 +0200

    adapt after changes in simulation

--- DonkeyKong/Game/main.cpp
+++ DonkeyKong/Game/main.cpp
@@ -4,13 +4,13 @@ using namespace std;
 
 int main(void){
     Jumpman jumpman;
-    Simulation simulation(&jumpman,1700,1500);
+    Simulation simulation(&jumpman,1700,1500,true);
     simulation.loadMapFromJson(string(PROJECTPATH)+"/map.json");
     Renderer renderer(simulation,1700,1500);
 
     while (renderer.isOpen())
     {
-        simulation.run();
+        (void)simulation.run();
         renderer.run();
     }
     
--- DonkeyKong/Game/main.py
+++ DonkeyKong/Game/main.py
@@ -1,8 +1,11 @@
 import build.Debug.DonkeyKongPy as DK
 jm = DK.Jumpman() 
-sim = DK.Simulation(jm,1700,1500)
+sim = DK.Simulation(jm,1700,1500,False)
 sim.loadMapFromJson("map.json")
 renderer = DK.Renderer(sim,1700,1500)
 while renderer.isOpen():
-    sim.run()
+    done = bool(sim.run())
+    a = sim.getBarrelsPositions()
+    b = sim.getJumpmanPosition()
     renderer.run()
+    print(done)
