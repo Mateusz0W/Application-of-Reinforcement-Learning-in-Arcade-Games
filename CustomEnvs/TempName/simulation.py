9db74996882323e8e4c824ed4442ef609063507a
Mateusz0W <153832537+Mateusz0W@users.noreply.github.com>
2025-10-11 18:26:06 +0200

    added obstacles and reset method

--- CustomEnvs/TempName/simulation.py
+++ CustomEnvs/TempName/simulation.py
@@ -1,15 +1,44 @@
 from player import Player
 from config import Colors
+from obstacle import Obstacle
+from config import GameConfig, ObstacleConfig, Colors
+import random
+
 class Simulation:
     def __init__(self) -> None: 
         self.players = [
             Player(500,250,20,20,5,Colors.Blue),
             Player(500,750,20,20,5,Colors.Red)
         ]
-    def run(self):
-        self.update()
+        self.obstacles = self._set_obstacles()
 
-    def update(self):
+    def run(self) -> None:
+        self.update()
+        self.reset()
+
+    def update(self) -> None:
         direction = Player.keyboard_input()
         if direction:
+            self.players[1].check_collision(self.players[0])
+            for obstacle in self.obstacles:
+                self.players[1].check_collision(obstacle)
+            #print(self.players[1].collision_side)
             self.players[1].update_position(direction)
+
+    def _set_obstacles(self) -> list[Obstacle]:
+        obstacles = []
+        for _ in range(GameConfig.num_of_obstacles):
+            x = random.randint(0, GameConfig.screen_width)
+            y = random.randint(0, GameConfig.screen_height)
+
+            width = random.randint(ObstacleConfig.min_width, ObstacleConfig.max_width)
+            height = random.randint(ObstacleConfig.min_height, ObstacleConfig.max_height)
+
+            obstacles.append(Obstacle(x, y, width, height, Colors.Green))
+        
+        return obstacles
+    
+    def reset(self) -> None:
+        for player in self.players:
+            player.reset()
+
