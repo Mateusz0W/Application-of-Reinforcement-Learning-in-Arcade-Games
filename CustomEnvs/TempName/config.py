a5f0509c664dc1dd043f59005d813ed3c12787bc
Mateusz0W <153832537+Mateusz0W@users.noreply.github.com>
2025-10-11 18:24:02 +0200

    added config for obstacles

--- CustomEnvs/TempName/config.py
+++ CustomEnvs/TempName/config.py
@@ -12,10 +12,20 @@ class GameConfig:
     screen_width: int = 1000
     screen_height: int = 800
     fps: int = 60
+    num_of_obstacles: int = 10
 
 @dataclass(frozen=True)
 class Colors:
     Red = (255, 0, 0)
     Blue = (0, 0, 255)
     Black = (0, 0, 0)
+    Green = (0, 255, 0)
+
+@dataclass(frozen=True)
+class ObstacleConfig:
+    min_width: int = 10
+    max_width: int = 70
+    max_height: int = 70
+    min_height: int = 10
+
 
