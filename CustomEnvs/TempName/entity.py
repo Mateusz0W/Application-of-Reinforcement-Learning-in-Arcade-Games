bd23bb8def5eba167b52d1ff5f329bb645b5967d
Mateusz0W <153832537+Mateusz0W@users.noreply.github.com>
2025-10-11 11:48:26 +0200

    created entity class for collision detection

---
+++ CustomEnvs/TempName/entity.py
@@ -0,0 +1,45 @@
+from config import Direction
+
+class Entity:
+    def __init__(self, x: float, y: float, width: float, height: float):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.collision = False
+        self.collision_side = None
+
+    def _check_box_collision(self, other: "Entity") -> bool:
+        return (
+            self.x  < other.x + other.width and 
+            self.x + self.width > other.x and
+            self.y < other.y + other.height and
+            self.y + self.height > other.y
+        )
+        
+    def _check_side_of_collision(self, other: "Entity") -> Direction:
+        right = abs((self.x + self.width) - other.x)
+        left = abs(self.x - (other.x + other.width))
+        down = abs((self.y + self.height) - other.y)
+        up = abs(self.y - (other.y + other.height))
+
+
+        collision_side =  min(right, left, up, down)
+
+        if collision_side == right:
+            return Direction.RIGHT
+        elif collision_side == left:
+            return Direction.LEFT
+        elif collision_side == up:
+            return Direction.UP
+        elif collision_side == down:
+            return Direction.DOWN
+        
+    def check_collision(self, other) -> None:
+        if not self._check_box_collision(other):
+            self.collision = False 
+            self.collision_side = None
+            return
+        
+        self.collision = True
+        self.collision_side = self._check_side_of_collision(other)
