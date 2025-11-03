6b920875a48fc522325acbac59b9c61c9a1a3baa
Mateusz0W <153832537+Mateusz0W@users.noreply.github.com>
2025-11-03 19:27:55 +0100

    raname TempName to Shooter

---
+++ CustomEnvs/Shooter/test.py
@@ -0,0 +1,13 @@
+import gymnasium as gym
+import environment  # <- to ładuje i rejestruje env
+
+env = gym.make("Shooter-v0",render_mode='human')
+obs, info = env.reset()
+
+done = False
+r = 0
+while True:
+    action = env.action_space.sample()
+    obs, reward, done, truncated, info = env.step(action)
+    r += reward
+    print(f"Reward: {r}")
