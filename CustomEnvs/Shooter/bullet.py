5ed04b670b8e3c8aa42731873c041b0bf5bc461b
Mateusz0W <153832537+Mateusz0W@users.noreply.github.com>
2025-11-03 20:10:59 +0100

    Revert "Delete CustomEnvs/Shooter directory"
    This reverts commit cc2fb59cb7111ff72ee0fc42f7a0949d0604938a.

---
+++ CustomEnvs/Shooter/bullet.py
@@ -0,0 +1,48 @@
+from entity import Entity
+from config import GameConfig, Direction
+import math
+import pygame
+
+class Bullet(Entity):
+    
+    def __init__(self, x: float, y: float, radius: float, color: tuple[int, int, int], speed: float, angle: float, life: int, width: float = None, height: float = None):
+        super().__init__(x, y, width, height, radius)
+        self.color = color
+        self.speed_x = speed
+        self.speed_y = speed
+        self.angle = angle
+        self.life = life
+
+    def update_position(self) -> None:
+        self.x += self.speed_x * math.cos(math.radians(self.angle))
+        self.y += self.speed_y * math.sin(math.radians(self.angle))
+
+    def draw(self, screen) -> None:
+        pygame.draw.circle(screen, self.color, (self.x, self.y), self.radius)
+
+    def update(self,others) -> None:
+        for other in others:
+            self.check_collision(other)
+            if self.collision:
+                from player import Player
+                if isinstance(other, Player):
+                    other.hit_by_bullet = True
+                    return
+                if self.collision_side in (Direction.RIGHT, Direction.LEFT):
+                    self.speed_x *= -1
+                else:
+                    self.speed_y *= -1
+                    
+                self.life -= 1
+                break
+        
+        if not 0 <= self.x <= GameConfig.screen_width:
+            self.speed_x *= -1
+
+        if not 0 <= self.y <= GameConfig.screen_height:
+            self.speed_y *= -1
+
+        self.update_position()
+
+        
+
---
+++ CustomEnvs/Shooter/simulation.py
@@ -0,0 +1,102 @@
+from player import Player
+from config import Colors
+from obstacle import Obstacle
+from config import GameConfig, ObstacleConfig, Colors, Direction
+import random
+import pygame
+import time
+from renderer import  Renderer
+
+class Simulation:
+    def __init__(self, render: bool=False, debuging: bool=False) -> None: 
+        self.players = [
+            Player(500,250,20,20,5,Colors.Blue),
+            Player(500,750,20,20,5,Colors.Red)
+        ]
+        self.obstacles = self._set_obstacles()
+        self.bullets = []
+        self.game_over = False
+        self.renderer = Renderer(render)
+        self.running = True
+        self.image = None
+        self.debuging = debuging
+
+    def run(self, action: int) -> bool:
+        self.update(action)
+        self.reset_collision_flags()
+        if self.debuging:
+            self.restart_game()
+        self.running, self.image = self.renderer.render(self,self.running)
+        return self.running
+
+    def update(self, action: int) -> None:
+        current_time = time.time()
+        if self.debuging:
+            direction, space_pressed = self._keyboard_input()
+            angle = 120
+        else:
+            if isinstance(action, Direction):
+                direction = action
+                space_pressed = False
+            else:
+                direction = None
+                space_pressed = True
+                angle = action
+        self.players[1].check_collision(self.players[0])
+        for obstacle in self.obstacles:
+            self.players[1].check_collision(obstacle)
+        if direction:
+            self.players[1].update_position(direction)
+        if self.players[1].reload(current_time):
+            if space_pressed:
+                self.bullets.append(self.players[1].shoot(angle, current_time))
+        for bullet in self.bullets:
+            bullet.update(self.players + self.obstacles)
+            if any(player.hit_by_bullet for player in self.players):
+                self.game_over = True
+                break
+    
+        self.delete_bullets()
+
+    def _set_obstacles(self) -> list[Obstacle]:
+        obstacles = []
+        for _ in range(GameConfig.num_of_obstacles):
+            x = random.randint(0, GameConfig.screen_width)
+            y = random.randint(0, GameConfig.screen_height)
+
+            width = random.randint(ObstacleConfig.min_width, ObstacleConfig.max_width)
+            height = random.randint(ObstacleConfig.min_height, ObstacleConfig.max_height)
+
+            obstacles.append(Obstacle(x, y, width, height, Colors.Green))
+        
+        return obstacles
+    
+    def reset_collision_flags(self) -> None:
+        for obj in self.players + self.bullets:
+            obj.reset_collision_flags()
+
+    def delete_bullets(self) -> None:
+        for idx, bullet in enumerate(self.bullets):
+            if bullet.life <= 0:
+                del self.bullets[idx]
+
+    def restart_game(self) -> None:
+        if not self.game_over:
+            return
+        
+        self.players = [
+            Player(500,250,20,20,5,Colors.Blue),
+            Player(500,750,20,20,5,Colors.Red)
+        ]
+        self.bullets.clear()
+        self.game_over = False
+
+    def _keyboard_input(self) -> tuple:
+        keys = pygame.key.get_pressed()
+        direction = Player.keyboard_input()
+        space_pressed = True if keys[pygame.K_SPACE] else False
+
+        return direction, space_pressed
+
+
+
