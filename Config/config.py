cf45484774954817f8366116cab83ae5953a67ef
Mateusz0W <153832537+Mateusz0W@users.noreply.github.com>
2025-11-22 12:29:54 +0100

    add str method to Hyperparameters

--- Config/config.py
+++ Config/config.py
@@ -13,5 +13,18 @@ class Hyperparameters:
     EPSILON_START: float
     EPSILON_FINAL: float
 
+    def __str__(self):
+        return f"Hyperparameters:\n\
+        Mean reward bound = {self.MEAN_REWARD_BOUND}\n\
+        Gamma = {self.GAMMA}\n\
+        Replay size = {self.REPLAY_SIZE}\n\
+        Replay start size = {self.REPLAY_START_SIZE}\n\
+        Learning rate = {self.LEARNING_RATE}\n\
+        Sync target frames = {self.SYNC_TARGET_FRAMES}\n\
+        Epsilon decay last frame = {self.EPSILON_DECAY_LAST_FRAME}\n\
+        Epsilon start = {self.EPSILON_START}\n\
+        Epsilon final = {self.EPSILON_FINAL}\n\
+        "
+
 
 
