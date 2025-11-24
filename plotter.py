274edb4dbd5e24d0ad697c77c276aa78b41dd969
Mateusz0W <153832537+Mateusz0W@users.noreply.github.com>
2025-11-24 13:57:46 +0100

    reimplement plotter

--- plotter.py
+++ plotter.py
@@ -1,29 +1,150 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
+import os
 
 class Plotter:
+    def __init__(self, title: str):
+        self.rewards = []
+        self.mean_rewards = []
+        self.steps = []
+        self.episode_lengths = []
+        self.losses = []
+        self.hours = []
+        self.fps = []
+        self.steps_loss = []
+        self.q_vals = []
+        self.title = title
+        self.fname = f"Plots/{self.title}"
 
-    @staticmethod
-    def plot(rewards, frame_idx, speed, title):
-        fig, axs = plt.subplots(2, 2, figsize=(10, 8))
+    def _moving_average(data, window_size=100):
+        if len(data) < window_size:
+            return data
+        return np.convolve(data, np.ones(window_size)/window_size, mode='valid')
 
-        episodes = list(range(1, len(rewards)+1))
-        mean_rewards = [np.mean(rewards[-100:i]) for i in range(len(rewards))]
-        std_rewards = [np.std(rewards[-100:i]) for i in range(len(rewards))]
+    def _add_hours_to_x_axis(ax, hours: list[float]) -> None:  
+        if len(hours) > 0:
+            ax_time = ax.twiny()
+            ax_time.set_xlim(ax.get_xlim()) 
 
-        axs[0,0].plot(frame_idx, mean_rewards)
-        axs[0,0].set_title("Mean rewards")
+            num_ticks = 5
+            if len(hours) < num_ticks:
+                return 
+            
+            ticks_indices = np.linspace(0, len(hours)-1, num=num_ticks).astype(int)    
+            ax_time.set_xticks(ticks_indices)
+            ax_time.set_xticklabels([f"{hours[t]:.1f}" for t in ticks_indices])
+            ax_time.set_xlabel("Godziny")
 
-        axs[0,1].plot(frame_idx, std_rewards)
-        axs[0,1].set_title("Std")
+    def _set_legends(ax, title: str, x_label: str, y_label: str) -> None:
+        ax.set_title(title)
+        ax.set_xlabel(x_label)
+        ax.set_ylabel(y_label)
+        ax.grid(True, alpha=0.3)
+        handles, labels = ax.get_legend_handles_labels()
+        if labels:
+            ax.legend(loc='upper left')
 
-        axs[1,0].plot(frame_idx, speed)
-        axs[1,0].set_title("speed")
+    def save_data(self, **kwargs) -> None:
+        for key, value in kwargs.items():
+            if hasattr(self, key) and value is not None:
+                    getattr(self, key).append(value)
 
-        axs[1,1].plot(frame_idx, rewards)
-        axs[1,1].set_title("rewards")
+    def _save_to_csv(self):
+        data = self.__dict__
+        data = {k: v for k, v in self.__dict__.items() if isinstance(v, list)}
+        max_len = max(len(values) for values in data.values())
 
+        for _, v in data.items():
+            if len(v) < max_len:
+                v.extend([np.nan] * (max_len - len(v)))
+
+        df = pd.DataFrame(data)
+        df.to_csv(f"{self.fname}/{self.title}.csv", index=False, sep=';')
+
+    def _plot(self):
+        os.makedirs(self.fname, exist_ok=True)
+
+        fig_names = ["rewards", "loss", "ep_len", "ep_len_hours", "fps", "loss_steps", "mean_reward", "q_vals"]
+        fig, axs = plt.subplots(4, 2, figsize=(16, 16))
+
+        # --- 1. Rewards (per Episode) ---
+        axs[0,0].plot(self.rewards, alpha=0.3, color='gray', label='Dane surowe')
+        if len(self.rewards) > 0:
+            ma_rewards = Plotter._moving_average(self.rewards)
+            axs[0,0].plot(np.arange(len(self.rewards)-len(ma_rewards), len(self.rewards)), ma_rewards, color='blue', label='Średnia (100)')
+        Plotter._set_legends(axs[0,0],"Nagorda na epizod", "Epizod", "Nagroda") 
+        Plotter._add_hours_to_x_axis(axs[0,0], self.hours)
+
+        # --- 2. Loss (per Training Step) ---
+        #axs[0,1].plot(self.losses, alpha=0.3, color='red', label='Dane surowe')
+        if len(self.losses) > 0:
+            sampled_loss = Plotter._moving_average(self.losses, window_size=1000)
+            axs[0,1].plot(np.arange(len(self.losses)-len(sampled_loss), len(self.losses)), sampled_loss, color='darkred', label=f'Średnia (1000)')
+        Plotter._set_legends(axs[0,1], "Strata (Wygładzona)", "Kroki treningowe (≈ x1000)", "Nagroda")
+
+        # --- 3. Episode Length (per Episode) ---
+        if len(self.episode_lengths) > 0:
+            ma_len = Plotter._moving_average(self.episode_lengths)
+            axs[1,0].plot(self.episode_lengths, alpha=0.3, color='gray' ,label='Dane surowe')
+            if len(ma_len) > 0:
+                axs[1,0].plot(np.arange(len(self.episode_lengths)-len(ma_len), len(self.episode_lengths)), ma_len, color='darkorange',label='Średnia (100)')
+        Plotter._set_legends(axs[1,0], "Długość epizodu (kroki)", "Epizod", "Kroki")
+        Plotter._add_hours_to_x_axis(axs[1,0], self.hours)
+
+        # --- 4. Episode Length vs Hours ---
+        if len(self.episode_lengths) > 0:
+            ma_len = Plotter._moving_average(self.episode_lengths)
+            if len(ma_len) > 0:
+                axs[1,1].plot(np.arange(len(self.episode_lengths)-len(ma_len), len(self.episode_lengths)), ma_len, color='limegreen',label='Średnia (100)')
+        Plotter._set_legends(axs[1,1], "Średnia długości epizodu w czasie", "Godziny", "Kroki w epizodzie")
+        Plotter._add_hours_to_x_axis(axs[1,0], self.hours)
+
+        # --- 5. FPS vs Steps ---
+        axs[2,0].plot(self.steps, self.fps, alpha=0.6, color='black')
+        Plotter._set_legends(axs[2,0], "Prędkość treningu", "Całkowita liczba klatek (Steps)", "FPS")
+
+        # --- 6. Loss vs Steps ---
+        axs[2,1].plot(self.steps_loss, self.losses,alpha=0.6, color='olive')
+        Plotter._set_legends(axs[2,1], "Strata względem kroków", "Kroki", "Strata")
+
+        # --- 7. Mean Rewards vs Steps ---
+        axs[3,0].plot(self.steps, self.mean_rewards, alpha=0.6, color='purple')
+        Plotter._set_legends(axs[3,0],"Średnia nagroda (ostatnie 100)", "Kroki", "Średnia Nagroda")
+
+        # --- 8. Q_val vs Steps ---
+        axs[3,1].plot(self.steps_loss, self.q_vals, alpha=0.3, color='gray', label='Dane surowe')
+        if len(self.q_vals) > 0:
+            window_q = 500 
+            sampled_q = Plotter._moving_average(self.q_vals, window_size=window_q)
+            axs[3,1].plot(self.steps_loss[len(self.steps_loss)-len(sampled_q):], sampled_q, color='darkblue', linewidth=1.5, label=f'Średnia ({window_q})')
+        Plotter._set_legends(axs[3,1], "Średnia wartość Q (Estymacja)", "Kroki", "Wartość Q")
+
+        # --- Save ---
         plt.tight_layout()
-        plt.savefig(f"{title}.png")
-        plt.show()
+        plt.savefig(f"{self.fname}/{self.title}.png")
 
+        for idx, ax in enumerate(axs.flatten()):
+            fig_single, ax_single = plt.subplots(figsize=(8, 6))
+            for line in ax.get_lines():
+                ax_single.plot(*line.get_data(), color=line.get_color(), alpha=line.get_alpha(), label=line.get_label())
+            
+            ax_single.set_title(ax.get_title())
+            ax_single.set_xlabel(ax.get_xlabel())
+            ax_single.set_ylabel(ax.get_ylabel())
+            ax_single.grid(True, alpha=0.3)
+
+            handles, labels = ax.get_legend_handles_labels()
+            if labels:
+                ax_single.legend()
+
+            fig_single.tight_layout()
+            fig_single.savefig(f"{self.fname}/{fig_names[idx]}.png")
+            plt.close(fig_single)
+
+        plt.show() 
+        plt.close(fig)
+
+    def run(self):
+        self._plot()
+        self._save_to_csv()
--- train.py
+++ train.py
@@ -37,7 +37,7 @@ os.makedirs("models", exist_ok=True)
 
 metadata = {
     "DQNs": ["DQN", "Double_DQN", "PrioDQN", "DuelingDQN", "NoisyDQN", "NStepDQN"],
-    "Env": ["ALE/MsPacman-v5", "Shooter-v0"]
+    "Env": ["ALE/MsPacman-v5", "Shooter-v0", "ALE/Breakout-v5"]
     }
 
 def setup(alg, env, hyp: Hyp):
@@ -88,10 +88,10 @@ def process_batch_dqn(optimizer, buffer, dqn, batch_size, net, target_net, gamma
 
         optimizer.zero_grad()
         batch = buffer.sample(batch_size)
-        loss_t = dqn.calc_loss(batch, net, target_net, gamma, device)
+        loss_t, mean_q = dqn.calc_loss(batch, net, target_net, gamma, device)
         loss_t.backward()
         optimizer.step()
-        return loss_t.item()
+        return loss_t.item(), mean_q
 
 def process_batch_prio_dqn(optimizer, buffer, dqn, batch_size, net, target_net, gamma, device, frame_idx):
     batch, batch_indices, batch_weights = buffer.sample(batch_size)
@@ -110,32 +110,34 @@ def process_batch_prio_dqn(optimizer, buffer, dqn, batch_size, net, target_net, 
 
 
 if __name__ == "__main__":
-
     assert len(sys.argv) >= 3, "Error Missing arguments" 
 
     env_name = sys.argv[1]
     algorithm = sys.argv[2]
     algorithm_param = sys.argv[3]
+    fname = f"models/{algorithm}"
+    os.makedirs(fname, exist_ok=True)
+    plot = Plotter(algorithm) 
 
     assert env_name in metadata["Env"], "Error: Wrong env name"
     assert algorithm in metadata["DQNs"], "Error: Wrong algortihm name"
     #assert algorithm == "NStepDQN" and len(sys.argv) != 4 and int(algorithm_param) < 0, "Error: Missing n argument or n must be non-negative"
 
     gym.register_envs(ale_py)
-    if env_name == "ALE/MsPacman-v5":
+    if env_name == "ALE/MsPacman-v5": #"ALE/MsPacman-v5":
         env = Wrappers.make_env(env_name)
     else:
         env = Wrappers.make_env(env_name, render_mode='human')
         #env = Wrappers.make_env(env_name)
     hyp = Hyp(
-        MEAN_REWARD_BOUND = 1_000,
+        MEAN_REWARD_BOUND = 540,
         GAMMA = 0.99 if algorithm != "NStepDQN" else 0.99 ** int(algorithm_param),
         BATCH_SIZE = 32,
         REPLAY_SIZE = 10_000,
         REPLAY_START_SIZE = 10_000,
         LEARNING_RATE = 1e-4,
         SYNC_TARGET_FRAMES = 1_000,
-        EPSILON_DECAY_LAST_FRAME = 75_000, #150_000
+        EPSILON_DECAY_LAST_FRAME = 150_000,
         EPSILON_START = 1.0,
         EPSILON_FINAL = 0.01
     )
@@ -148,23 +150,24 @@ if __name__ == "__main__":
     ts = time.time()
     best_mean_reward = None 
     print(f"device = {device}\nnetwork = {net}")
+    print(hyp)
     start_time = time.time()
-    loss = []
-    fps = []
-    steps = []
+    current_ep_steps = 0
 
     while True:
+        current_ep_steps += 1
         frame_idx += 1
         epsilon = max(hyp.EPSILON_FINAL, hyp.EPSILON_START - frame_idx / hyp.EPSILON_DECAY_LAST_FRAME)
         reward = agent.play_step(net, epsilon, device=device)
         if reward is not None:
             total_rewards.append(reward)
             speed = (frame_idx - ts_frame) / (time.time() - ts)
-            fps.append(speed)
-            steps.append(frame_idx)
             ts_frame = frame_idx
             ts = time.time()
+            hour = (ts - start_time) / 3600
             mean_reward = np.mean(total_rewards[-100:])
+            plot.save_data(rewards=reward, mean_rewards=mean_reward, steps=frame_idx, episode_lengths=current_ep_steps, hours=hour, fps=speed)
+            current_ep_steps = 0
             print(f"\r{frame_idx}: done {len(total_rewards)} games, reward {mean_reward:.3f}, eps {epsilon:.2f}, speed {speed:.2f} f/s", end="", flush=True)
             writer.add_scalar("epsilon", epsilon, frame_idx)
             writer.add_scalar("speed", speed, frame_idx)
@@ -172,7 +175,7 @@ if __name__ == "__main__":
             writer.add_scalar("reward", reward, frame_idx)
 
             if best_mean_reward is None or best_mean_reward < mean_reward:
-                torch.save(net.state_dict(), f"models/{env_name}-best_{mean_reward}_{int(time.time())}.dat")
+                torch.save(net.state_dict(), f"{fname}/{env_name[3:] if 'ALE/' in env_name else env_name}-best_{mean_reward}_{int(time.time())}.dat")
 
                 if best_mean_reward is not None:
                     print("\nBest reward updated %.3f -> %.3f" % (best_mean_reward, mean_reward))
@@ -186,8 +189,10 @@ if __name__ == "__main__":
         if len(buffer) < hyp.REPLAY_START_SIZE:
             continue
 
-        loss_t = process_batch(optimizer, buffer, dqn, hyp.BATCH_SIZE, net, target_net, hyp.GAMMA, device, frame_idx)
-        loss.append(loss_t)
+        loss_t, mean_q = process_batch(optimizer, buffer, dqn, hyp.BATCH_SIZE, net, target_net, hyp.GAMMA, device, frame_idx)
+        plot.save_data(losses=loss_t, steps_loss=frame_idx, q_vals=mean_q)
 
     total_time = (time.time() - start_time) / 3600
-    Plotter.plot(total_rewards,steps,fps,str(algorithm))
+    writer.close()
+    print(f"total learnig time = {total_time}")
+    plot.run()
