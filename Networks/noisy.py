9a82cfa122f315b18147358dd288cff38d51f7f6
Mateusz0W <153832537+Mateusz0W@users.noreply.github.com>
2025-12-22 19:21:17 +0100

    add NoisyDQN class

--- Networks/noisy.py
+++ Networks/noisy.py
@@ -3,10 +3,10 @@ import torch.nn as nn
 import numpy as np
 from torch.nn import functional as F
 
-class Noisy(nn.Linear):
+class NoisyLinear(nn.Linear):
     def __init__(self, in_features, out_features, bias = True, sigma_init=0.017):
         super().__init__(in_features, out_features, bias)
-        w = torch.full((in_features,out_features), sigma_init)
+        w = torch.full((out_features, in_features), sigma_init)
         self.sigma_weight = nn.Parameter(w)
         z = torch.zeros(out_features, in_features)
         self.register_buffer("epsilon_weight", z)
@@ -31,3 +31,33 @@ class Noisy(nn.Linear):
         v = self.sigma_weight * self.epsilon_weight.data + self.weight
         return F.linear(input, v, bias)
 
+
+class NoisyDQN(nn.Module):
+    def __init__(self, input_shape, n_actions):
+        super(NoisyDQN, self).__init__()
+        
+        self.conv = nn.Sequential(
+            nn.Conv2d(input_shape[0], 32, kernel_size=8, stride=4),
+            nn.ReLU(),
+            nn.Conv2d(32, 64, kernel_size=4, stride=2),
+            nn.ReLU(),
+            nn.Conv2d(64, 64, kernel_size=3, stride=1),
+            nn.ReLU()
+        )
+
+        self.conv_out_size = self._get_conv_out(input_shape)
+
+        self.noisy_layers = nn.Sequential(
+            NoisyLinear(self.conv_out_size, 512),
+            nn.ReLU(),
+            NoisyLinear(512, n_actions)
+        )
+
+    def _get_conv_out(self, shape):
+        o = self.conv(torch.zeros(1, *shape))
+        return int(np.prod(o.size()))
+
+    def forward(self, x):
+        fx = x.float() / 256
+        conv_out = self.conv(fx).view(fx.size()[0], -1)
+        return self.noisy_layers(conv_out)
